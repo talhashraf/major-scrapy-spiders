ede85cf603adc69e52bf32bb7a532cc999cf7862
Talha Ashraf <talhashraf@gmail.com>
2017-07-20 22:19:45 +0500

    update instagram spider

--- mss/spiders/instagram.py
+++ mss/spiders/instagram.py
@@ -1,43 +1,17 @@
-import re
+'''MSS: Instagram Spider'''
 import json
 
-from scrapy import Spider, Item, Field
+from scrapy import Spider
 
-from mss.utils import get_extracted
-
-
-class InstagramProfileItems(Item):
-    is_private = Field()
-    posts = Field()
-    username = Field()
-    bio = Field()
-    website = Field()
-    profile_picture = Field()
-    full_name = Field()
-    total_posts = Field()
-    followers = Field()
-    following = Field()
 
 class Instagram(Spider):
-    name = "Instagram"
-    start_urls = ["http://instagram.com/nike/"]
-
-    download_delay = 0.5
+    '''MSS: Instagram Spider'''
+    name = 'Instagram'
+    start_urls = [
+        'https://www.instagram.com/nike/',
+    ]
 
     def parse(self, response):
-        javascript = "".join(response.xpath('//script[contains(text(), "sharedData")]/text()').extract())
-        json_data = json.loads("".join(re.findall(r'window._sharedData = (.*);', javascript)))
-
-        item = InstagramProfileItems()
-        data = get_extracted(json_data["entry_data"]["UserProfile"])
-        item["is_private"] = data["relationship"]["is_private"]
-        item["posts"] = data["userMedia"]
-        item["username"] = data["user"]["username"]
-        item["bio"] = data["user"]["bio"]
-        item["website"] = data["user"]["website"]
-        item["profile_picture"] = data["user"]["profile_picture"]
-        item["full_name"] = data["user"]["full_name"]
-        item["total_posts"] = data["user"]["counts"]["media"]
-        item["followers"] = data["user"]["counts"]["followed_by"]
-        item["following"] = data["user"]["counts"]["follows"]
-        return item
+        '''Parse profile JSON data'''
+        json_string = response.xpath('//script/text()').re(r'sharedData = (.+);')[0]
+        return json.loads(json_string)
